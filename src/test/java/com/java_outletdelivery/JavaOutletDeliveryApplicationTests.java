a6e0eb3ad1c4b464a133f5c3ab24f4244fad77a3
adebusoyeteeman@gmail.com <adebusoyeteeman@gmail.com>
2024-08-17 12:23:49 +0100

    Spring boot test application

---
+++ src/test/java/com/java_outletdelivery/JavaOutletDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_outletdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaOutletDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
