268c45776639b8987c7a97a36e909fd74123c72a
adebusoyeteeman@gmail.com <adebusoyeteeman@gmail.com>
2024-08-17 12:23:11 +0100

    Spring boot main application

---
+++ src/main/java/com/java_outletdelivery/JavaOutletDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.java_outletdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOutletDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaOutletDeliveryApplication.class, args);
+    }
+
+}
